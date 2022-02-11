33b2f332bea8fd845260bad50ba802a34b7e7e94
ArtMinerCZ <filip.kupsky123@gmail.com>
2022-02-11 11:01:33 +0100

    bronze knife

---
+++ src/main/java/net/mcreator/craftindustry/item/KnifebronzeItem.java
@@ -0,0 +1,59 @@
+
+package net.mcreator.craftindustry.item;
+
+import net.minecraft.world.item.crafting.Ingredient;
+import net.minecraft.world.item.Tier;
+import net.minecraft.world.item.SwordItem;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Item;
+
+public class KnifebronzeItem extends SwordItem {
+	public KnifebronzeItem() {
+		super(new Tier() {
+			public int getUses() {
+				return 100;
+			}
+
+			public float getSpeed() {
+				return 4f;
+			}
+
+			public float getAttackDamageBonus() {
+				return 0f;
+			}
+
+			public int getLevel() {
+				return 1;
+			}
+
+			public int getEnchantmentValue() {
+				return 5;
+			}
+
+			public Ingredient getRepairIngredient() {
+				return Ingredient.EMPTY;
+			}
+		}, 3, 21f, new Item.Properties().tab(null));
+		setRegistryName("knifebronze");
+	}
+
+	@Override
+	public boolean hasContainerItem(ItemStack stack) {
+		return true;
+	}
+
+	@Override
+	public ItemStack getContainerItem(ItemStack itemstack) {
+		ItemStack retval = new ItemStack(this);
+		retval.setDamageValue(itemstack.getDamageValue() + 1);
+		if (retval.getDamageValue() >= retval.getMaxDamage()) {
+			return ItemStack.EMPTY;
+		}
+		return retval;
+	}
+
+	@Override
+	public boolean isRepairable(ItemStack itemstack) {
+		return false;
+	}
+}
