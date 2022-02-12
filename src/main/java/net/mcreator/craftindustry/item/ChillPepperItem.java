fae4bbc33aaa5c06212c94d668b696e790a76bf1
porilter <miki.krchnak@gmail.com>
2022-02-12 11:05:00 +0100

    Filda je magor

--- src/main/java/net/mcreator/craftindustry/item/ChillPepperItem.java
+++ src/main/java/net/mcreator/craftindustry/item/ChillPepperItem.java
@@ -5,15 +5,15 @@ import net.minecraft.world.level.Level;
 import net.minecraft.world.item.Rarity;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.food.FoodProperties;
 import net.minecraft.world.entity.LivingEntity;
 
 import net.mcreator.craftindustry.procedures.ChillPepperEatenProcedure;
-import net.mcreator.craftindustry.init.CraftindustryModTabs;
 
 public class ChillPepperItem extends Item {
 	public ChillPepperItem() {
-		super(new Item.Properties().tab(CraftindustryModTabs.TAB_CRAFT_INDUSTRY_COOKING).stacksTo(64).rarity(Rarity.COMMON)
+		super(new Item.Properties().tab(CreativeModeTab.TAB_FOOD).stacksTo(64).rarity(Rarity.COMMON)
 				.food((new FoodProperties.Builder()).nutrition(2).saturationMod(0.5f)
 
 						.build()));
--- src/main/java/net/mcreator/craftindustry/item/OnionItem.java
+++ src/main/java/net/mcreator/craftindustry/item/OnionItem.java
@@ -3,13 +3,12 @@ package net.mcreator.craftindustry.item;
 
 import net.minecraft.world.item.Rarity;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.food.FoodProperties;
-
-import net.mcreator.craftindustry.init.CraftindustryModTabs;
 
 public class OnionItem extends Item {
 	public OnionItem() {
-		super(new Item.Properties().tab(CraftindustryModTabs.TAB_CRAFT_INDUSTRY_COOKING).stacksTo(64).rarity(Rarity.COMMON)
+		super(new Item.Properties().tab(CreativeModeTab.TAB_FOOD).stacksTo(64).rarity(Rarity.COMMON)
 				.food((new FoodProperties.Builder()).nutrition(3).saturationMod(0.3f)
 
 						.build()));
